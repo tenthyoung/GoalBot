cc306af2033efa52ccdced550744b02d1ce90e41
jessica <jessicagomez11@live.com>
2019-06-08 14:22:17 -0700

    user model created

--- models/example.js
+++ models/example.js
@@ -1,13 +1,31 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("User", {
-  // var User = sequelize.define("User", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-
-  return Example;
-  // return User;
-};
+// module.exports = function(sequelize, DataTypes) {
+//   var Example = sequelize.define("Example", {
+//     text: DataTypes.STRING,
+//     description: DataTypes.TEXT
+//   });
+  
+//   return Example;
+// };
 
 
 //CHANGE THE NAME OF THE FILE TO USER.JS
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // Giving the User model a name of type STRING
+    userName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+   
+  });
+
+  User.associate = function(models) {
+    // Associating User with Posts
+    // When an User is deleted, also delete any associated Posts
+    User.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+
+  return User;
+};
