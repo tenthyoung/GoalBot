1a62405721013d796af138b4f8ab400c1a486049
Mohini Modi <Mohini.Modi@resmed.com>
2019-06-08 00:40:23 -0700

    database two tables completed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # project2
+
+Design:
+* Creating Database goalBotDB with two tables 
+1. userAccount (userName, pwd, avatar)
+2. goalInfo (goal, endDate, ms1, ms2, ms3, ms4, ms5)
+ 
--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,31 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS goalBotDB;
+CREATE database goalBotDB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE goalBotDB;
+
+CREATE TABLE user (
+user_id INT NOT NULL AUTO_INCREMENT,
+userName VARCHAR(100) NOT NULL,
+password VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+Avatar  LONGBLOB NOT NULL, 
+PRIMARY KEY (user_id)
+);
+
+
+
+CREATE TABLE goal(
+   goal_id int not null auto_increment primary key,
+   createDate datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   endDate datetime NOT NULL,
+   ms1 varchar(355) not null,
+   ms2 varchar(355) not null,
+   ms3 varchar(355) not null,
+   ms4 varchar(355) not null,
+   ms5 varchar(355) not null,
+   user_id int not null,
+   FOREIGN KEY fk_user(user_id)
+   REFERENCES user(user_id)
+   ON UPDATE CASCADE
+   ON DELETE RESTRICT
+)ENGINE=InnoDB;
