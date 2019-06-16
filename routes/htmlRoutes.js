dd693dd0db1d091d658d885788832235758da97d
jessica <jessicagomez11@live.com>
2019-06-15 18:16:51 -0700

    working functionality

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,49 +26,49 @@ module.exports = function (app) {
   });
 };
 
-// Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// // Requiring path to so we can use relative routes to our HTML files
+// var path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../public/js/isAuthenticated.js");
+// // Requiring our custom middleware for checking if a user is logged in
+// var isAuthenticated = require("../public/js/isAuthenticated.js");
 
-module.exports = function (app) {
+// module.exports = function (app) {
 
-  // Sign up
-  app.get("/", function (req, res) {
-    console.log(req.user)
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/userHome");
-    }
-    res.render("userHome");
-  });
+//   // Sign up
+//   app.get("/", function (req, res) {
+//     console.log(req.user)
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/userHome");
+//     }
+//     res.render("userHome");
+//   });
 
-  app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect("/");
-    // res.render("index")
-  });
+//   app.get('/logout', function (req, res) {
+//     // req.logout();
+//     res.redirect("/");
+//     // res.render("index")
+//   });
 
 
 
-  app.get("/login", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/userHome");
-    }
-    res.render("", "../public/login.html");
-  });
+//   app.get("/login", function (req, res) {
+//     // If the user already has an account send them to the members page
+//     if (req.user) {
+//       res.redirect("/userHome");
+//     }
+//     res.render("", "../public/login.html");
+//   });
 
-  app.get("/", function (req, res) {
-    res.render("", "../public/login.html");
-  });
+//   app.get("/", function (req, res) {
+//     res.render("", "../public/login.html");
+//   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function (req, res) {
-    res.render("", "../public/members.html");
-  });
+//   // Here we've add our isAuthenticated middleware to this route.
+//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
+//   app.get("/members", isAuthenticated, function (req, res) {
+//     res.render("", "../public/members.html");
+//   });
 
-};
+// };
 
